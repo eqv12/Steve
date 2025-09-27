9ee5c58fd10f1359b4d6c7724f8906b6344e0736
eqv12 <ramyaraja1206@gmail.com>
2025-09-27 14:09:16 +0530

    initial

---
+++ static/js/dino.js
@@ -0,0 +1,16 @@
+class Dino {
+  constructor(path, task, seqNum) {
+    // initialize properties
+    this.path = path;
+    this.task = task;
+    this.seqNum = seqNum
+  }
+
+  methodOne() {
+    // instance method
+  }
+
+  methodTwo(param) {
+    // use this.prop1 / this.prop2
+  }
+}
